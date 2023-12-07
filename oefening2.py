e546f6aca0bd34b426fd8cea548144c58005ff75
Willem <willem.delaeter@hotmail.com>
2023-12-07 20:25:54 +0100

    aangepast, som + 4

--- oefening2.py
+++ oefening2.py
@@ -3,7 +3,7 @@ getal1 = int(input(print("1e getal?")))
 
 getal2 = int(input(print("2e getal?")))
 
-som = getal1 + getal2
+som = getal1 + getal2 + 4
 product = getal1 * getal2
 
 print("De som is", som, "en het product is", product)
