8411dcb043d28b716cc2f4fce0248aee9d27aafe
Willem <willem.delaeter@hotmail.com>
2023-12-07 20:23:50 +0100

    nu wel met bestand bij

---
+++ oefening2.py
@@ -0,0 +1,9 @@
+
+getal1 = int(input(print("1e getal?")))
+
+getal2 = int(input(print("2e getal?")))
+
+som = getal1 + getal2
+product = getal1 * getal2
+
+print("De som is", som, "en het product is", product)
